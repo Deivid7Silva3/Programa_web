375a2615fc4801d06a306ebd27a06aab0a6244b3
Deivid7Silva3 <daividsilva341@gmail.com>
2023-04-11 19:48:28 -0300

    atualização

---
+++ script.js
@@ -0,0 +1,35 @@
+
+//Manipulação do DOM
+
+var inputQtde = document.querySelector('#qdte')
+inputQtde.addEventListener("change", atualizarOrcamento)
+
+document.querySelector('#Js').addEventListener("change", atualizarOrcamento)
+document.querySelector('#layout_sim').addEventListener("change", atualizarOrcamento)
+document.querySelector('#layout_nao').addEventListener("change", atualizarOrcamento)
+document.querySelector('#prazo').addEventListener("change", atualizarOrcamento)
+
+function atualizarOrcamento() {
+    let qtde = inputQtde.value
+    let preco = qtde * 100
+
+
+
+    let Js = document.querySelector('#Js').checked
+    if (Js) preco *= 1.1
+
+    let layout = document.querySelector("#layout_sim").checked
+    if (layout) preco *= 1 + (qtde * .1)
+
+    let prazo = document.querySelector("#prazo").value
+    let labelPrazo = document.querySelector("label[for=prazo]")
+    labelPrazo.innerHTML = `Prazo (${prazo} semanas)` //template litereal 
+    let taxaDeUrgencia = 1 - (prazo * .01);
+    preco *= taxaDeUrgencia
+
+    let output = document.querySelector('#secao-orcamento form output')
+    output.innerHTML = "R$ " + preco.toFixed(2)
+    console.log(Js)
+
+
+}
